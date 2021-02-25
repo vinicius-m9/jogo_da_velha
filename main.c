700ea2f10e7d67d7c9ed119f851979462f8bdf19
vinicius-m9 <viniciusmachadoribeiro@hotmail.com>
2021-02-25 20:12:56 -0300

    funcao para inicializar array adicionada

--- main.c
+++ main.c
@@ -1,6 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+char arr[3][3];
+
+void arrayInit(){
+    for(int i=0; i<3; i++){
+        for(int j=0; j<3; j++){
+            if(i==2){
+                arr[i][j] = ' ';
+                return;
+            }
+            arr[i][j] = '_';
+        }
+    }
+}
+
+
 int main(void){
+    arrayInit();
+
     return 0;
 }
