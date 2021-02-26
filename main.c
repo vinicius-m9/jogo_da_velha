9d5587deb346744dd7b7899ec4737eab8b7f56ab
vinicius-m9 <viniciusmachadoribeiro@hotmail.com>
2021-02-26 10:53:40 -0300

    funcao para printar array adicionada

--- main.c
+++ main.c
@@ -15,9 +15,29 @@ void arrayInit(){
     }
 }
 
+void printArray(){
+    int ASCII = 65;
+
+    printf("  1 2 3\n");
+
+    for(int i=0; i<3; i++){
+        printf("%c ", ASCII++);
+
+        for(int j=0; j<3; j++){
+            printf("%c", arr[i][j]);
+
+            if(j!=2)
+                printf("|");
+        }
+        printf("\n");
+    }
+    printf("\n");
+}
 
 int main(void){
     arrayInit();
+
+    printArray();
 
     return 0;
 }
