6913ec5697ebcf7176e1ef9b9e8ebf60cbcc67d2
vinicius-m9 <viniciusmachadoribeiro@hotmail.com>
2021-03-03 15:50:55 -0300

    algumas correcoes e funcoes changeArray, playerMove e playerXplayer adicionadas

--- main.c
+++ main.c
@@ -8,6 +8,8 @@
 #include <stdbool.h>
 
 char arr[3][3];
+char player1[51], player2[51];
+char symbol1, symbol2;
 
 void arrayInit(void){
     for(int i=0; i<3; i++){
@@ -28,11 +30,15 @@ int charToInt (char line){
             return 1;
         case 'C':
             return 2;
+        default:
+            break;
     }
     return -1;
 }
 
-int validChar (char symbol){
+int validSymbol(char symbol){
+    symbol = toupper(symbol);
+
     if(symbol == 'X' || symbol == 'O')
         return 1;
 
@@ -51,7 +57,7 @@ int validCoord(char line, int column){
 int coordEmpty(char line, int column){
     int i = charToInt(line), j = column-1;
 
-    if(!validChar(arr[i][j]))
+    if(!validSymbol(arr[i][j]))
         return 1;
 
     return 0;
@@ -62,7 +68,7 @@ int lineWin(void){
 
     for(int i=0; i<3; i++){
         for(int j=0; j<2; j++){
-            if(validChar(arr[i][j]) && arr[i][j] == arr[i][j+1])
+            if(validSymbol(arr[i][j]) && arr[i][j] == arr[i][j+1])
                 count++;
         }
         if(count == 2)
@@ -78,7 +84,7 @@ int columnWin(void){
 
     for(int j=0; j<3; j++){
         for(int i=0; i<2; i++){
-            if(validChar(arr[i][j]) && arr[i][j] == arr[i+1][j])
+            if(validSymbol(arr[i][j]) && arr[i][j] == arr[i+1][j])
                 count++;
         }
         if(count == 2)
@@ -94,7 +100,7 @@ int diagonalWin(void){
 
     // main diagonal
     for(int i=0; i<2; i++){
-        if(validChar(arr[i][i]) && arr[i][i] == arr[i+1][i+1])
+        if(validSymbol(arr[i][i]) && arr[i][i] == arr[i+1][i+1])
             count++;
     }
     if(count == 2)
@@ -104,7 +110,7 @@ int diagonalWin(void){
 
     // secondary diagonal
     for(int i=0; i<2; i++){
-        if(validChar(arr[i][i]) && arr[i][2-i] == arr[i+1][1-i])
+        if(validSymbol(arr[i][i]) && arr[i][2-i] == arr[i+1][1-i])
             count++;
     }
     if(count == 2)
@@ -139,7 +145,72 @@ void printArray(void){
     printf("\n");
 }
 
-int menuOption(void){
+void changeArray(char symbol, char line, int column){
+    int i = charToInt(line), j = column-1;
+
+    arr[i][j] = toupper(symbol);
+}
+
+void playerMove(char player[51], char symbol){
+    int column;
+    char line;
+
+    printf("Vez de %s. Indique linha e coluna para jogar\n", player);
+
+    printf("linha:\n");
+    scanf(" %c", &line);
+
+    printf("coluna:\n");
+    scanf("%i", &column);
+
+    while(!validCoord(line, column) || !coordEmpty(line, column)){
+        printf("Coordenada invalida. Digite novamente!\n");
+
+        printf("linha:\n");
+        scanf(" %c", &line);
+
+        printf("coluna:\n");
+        scanf("%i", &column);
+    }
+
+    changeArray(symbol, line, column);
+
+    /**
+    * OS variation
+    * system("cls"); -> Windows
+    * system("clear"); -> Linux
+    */
+    system("cls");
+}
+
+void playerXplayer(void){
+    int turn = 0;
+
+    while(!winner() && turn < 9){
+        printArray();
+
+        if(turn % 2 == 0)
+            playerMove(player1, symbol1);
+        else
+            playerMove(player2, symbol2);
+
+        turn++;
+    }
+
+    printArray();
+
+    // correção para identificar o ganhador
+    turn -= 1;
+
+    if(turn % 2 == 0)
+        printf("%s venceu!", player1);
+    else if(turn % 2 != 0)
+        printf("%s venceu!", player2);
+    else
+        printf("Deu velha!");
+}
+
+void menuOption(void){
     int option;
 
     printf("JOGO DA VELHA\n");
@@ -148,13 +219,44 @@ int menuOption(void){
 
     scanf("%i", &option);
 
-    /* OS variation
+    /**
+    * OS variation
     * system("cls"); -> Windows
     * system("clear"); -> Linux
     */
     system("cls");
 
-    return option;
+    if(option == 1){
+        printf("Digite o nome do jogador 1 (ex.: nome_sobrenome):\n");
+        scanf("%s", player1);
+
+        printf("\nDigite o simbolo que deseja utilizar: X ou O\n");
+        scanf(" %c", &symbol1);
+        printf("\n");
+
+        while(!validSymbol(symbol1)){
+            printf("simbolo invalido! Digite novamente:\n");
+            scanf(" %c", &symbol1);
+            printf("\n");
+        }
+
+        printf("Digite o nome do jogador 2 (ex.: nome_sobrenome):\n");
+        scanf("%s", player2);
+
+        if(toupper(symbol1) == 'X')
+            symbol2 = 'O';
+        else
+            symbol2 = 'X';
+
+        /**
+        * OS variation
+        * system("cls"); -> Windows
+        * system("clear"); -> Linux
+        */
+        system("cls");
+
+        playerXplayer();
+    }
 }
 
 int main(void){
